09e646004df9596554e978a43aad877b41fa04a4
Ammar Alakkad <am.alakkad@gmail.com>
2015-02-21 20:28:11 +0200

    Views - Posts - create: add form elements

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -6,20 +6,20 @@
 	<div class="row">
         <div class="col-md-8 col-md-offset-2">
             
-            <form class="form-horizontal" role="form" method="POST" action="/auth/register">
+            <form class="form-horizontal" role="form" method="POST" action="/posts">
                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
                 <div class="form-group">
-                    <label class="col-md-4 control-label">Name</label>
+                    <label class="col-md-4 control-label">Title</label>
                     <div class="col-md-6">
-                        <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                        <input type="text" class="form-control" placeholder="Title" name="title" value="{{ old('title') }}">
                     </div>
                 </div>
                 
                 <div class="form-group">
-                    <label class="col-md-4 control-label">Name</label>
+                    <label class="col-md-4 control-label">Content</label>
                     <div class="col-md-6">
-                        <textarea class="form-control" name="name">{{ old('name') }}</textarea>
+                        <textarea class="form-control" placeholder="Content" name="content">{{ old('content') }}</textarea>
                     </div>
                 </div>
                 
